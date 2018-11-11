e99f24a2fd5816908e4ed116b07586526803c20d
piyushrana <piyush.rana@hpe.com>
2018-11-11 15:18:35 -0800

    added test docker file and is able to create a image

--- src/main/scala/SearchMe.scala
+++ src/main/scala/SearchMe.scala
@@ -1,6 +1,36 @@
 package main.scala
 
+import scala.sys.process.Process
+
 object SearchMe extends App {
 
-  println("what do you want docker image or installed as service?")
+  var input: String = ""
+  do {
+    println("Get all the docker images self created right now-------")
+    println("Enter 1 for Test Image")
+    println("Enter 2 for Kakfa related images")
+    println("Enter q for Test Image")
+
+    input = readLine()
+
+    input match {
+      case "1" => {
+        println("Goona give you a test docker image .. Please enter the version you want-")
+        val input_for_version = readLine()
+        buildTestDockerImage(input_for_version)
+      }
+      case "2" => println("coming soon !!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+      case "q" => System.exit(0)
+      case _ => println("not a valid input-- please type what's in options!")
+    }
+  } while (input != "q")
+
+  def buildTestDockerImage(version : String) = {
+    val pathToTestImage = "/home/piyush/workstation/SoftwareCenter/src/main/scala/com/knoldus/docker/testdockerfile"
+    val testDockerImageCmd = s"docker build --build-arg CONT_IMG_VER=$version $pathToTestImage"
+    println("started building your docker image")
+    Thread.sleep(2000)
+    Process(testDockerImageCmd).lineStream_!
+    println("done.. docker images to view and docker run imageiD -it /bin/bash to get into it")
+  }
 }
