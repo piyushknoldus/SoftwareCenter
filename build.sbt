89d593ee838f28c18e404b57c7528ef580607b39
Anuj Saxena <anuj.saxena@knoldus.in>
2018-12-19 18:15:25 +0530

    Add configuration file to project  * add logging instead of println  * add gitignore for git

--- build.sbt
+++ build.sbt
@@ -1,5 +1,11 @@
+import Dependencies._
+
 name := "SoftwareCenter"
 
 version := "0.1"
 
 scalaVersion := "2.12.7"
+
+libraryDependencies ++= Seq(
+  typesafeConfig, log4j
+)
--- src/main/scala/com/knoldus/handlers/ImageCreation.scala
+++ src/main/scala/com/knoldus/handlers/ImageCreation.scala
@@ -1,43 +1,44 @@
 package com.knoldus.handlers
 
-import com.knoldus.utils.Constants
+import com.knoldus.utils.{Constants, DockerConfig, Logging}
+
 import scala.sys.process.Process
 
-object ImageCreation {
+object ImageCreation extends Logging{
 
   def buildTestDockerImage(userName : String, repoName:String, tagName:String): Boolean = {
-    val pathToTestImage = "/home/piyush/workstation/SoftwareCenter/src/main/scala/com/knoldus/docker/testdockerfile"
+    val pathToTestImage = DockerConfig.getValue("test.image")
     val testDockerImageCmd = s"""docker build -t $repoName:$tagName --build-arg USERNAME="$userName" $pathToTestImage"""
-    println(testDockerImageCmd)
+    debug(testDockerImageCmd)
     val res = try {
-      println(s"${Console.GREEN} ${Constants.started} ${Console.RESET}")
-      val res = Process(testDockerImageCmd).lineStream_!
-      println(s"${Console.GREEN} ${Constants.completed} ${Console.RESET}")
+      info(s"${Console.GREEN} ${Constants.started} ${Console.RESET}")
+      Process(testDockerImageCmd).lineStream_!.foreach(debug)
+      info(s"${Console.GREEN} ${Constants.completed} ${Console.RESET}")
       true
     }
     catch
       {
-        case ex:Exception => println(s"Something goes wrong!!! ${ex.printStackTrace()}")
+        case ex:Exception => error(s"Something goes wrong!!! ${ex.printStackTrace()}")
           false
       }
     res
   }
 
   def scalaDockerImage(scalaVersion : String,sbtVersion : String, repoName:String, tagName:String): Boolean = {
-    val pathToTestImage = "src/main/scala/com/knoldus/docker/scaladocker"
-    val testDockerImageCmd = s"docker build -t $repoName:$tagName --build-arg SCALA_VERSION=$scalaVersion,SBT_VERSION=$sbtVersion $pathToTestImage"
-      println(testDockerImageCmd)
+    val pathToTestImage = DockerConfig.getValue("scala.image")
+    val testDockerImageCmd = s"docker build -t $repoName:$tagName --build-arg SCALA_VERSION=$scalaVersion --build-arg SBT_VERSION=$sbtVersion $pathToTestImage"
+      debug(testDockerImageCmd)
     val res = try {
-      println(s"${Console.GREEN} ${Constants.started} ${Console.RESET}")
-      val res = Process(testDockerImageCmd).!!
+      info(s"${Console.GREEN} ${Constants.started} ${Console.RESET}")
+      Process(testDockerImageCmd).lineStream_!.foreach(info)
       true
     }
     catch
       {
-        case ex:Exception => println(s"Something goes wrong!!! ${ex.getMessage}")
+        case ex:Exception => error(s"Something goes wrong!!! ${ex.getMessage}")
           false
       }
-    println(s"${Console.GREEN} ${Constants.completed} ${Console.RESET}")
+    info(s"${Console.GREEN} ${Constants.completed} ${Console.RESET}")
     res
   }
 
